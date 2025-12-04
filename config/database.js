4cd81b543db583d92f302a839220467344a80b72
phillip847 <filippus224@gmail.com>
2025-12-04 18:12:46 +0200

    update

--- config/database.js
+++ config/database.js
@@ -3,26 +3,20 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const sequelize = process.env.DATABASE_URL 
-  ? new Sequelize(process.env.DATABASE_URL, {
-      dialect: 'postgres',
-      logging: false,
-      dialectOptions: {
-        ssl: {
-          require: true,
-          rejectUnauthorized: false
-        }
-      }
-    })
-  : new Sequelize(
-      process.env.DB_NAME || 'diary_investor',
-      process.env.DB_USER || 'root',
-      process.env.DB_PASSWORD || '',
-      {
-        host: process.env.DB_HOST || 'localhost',
-        dialect: process.env.DB_DIALECT || 'mysql',
-        logging: false,
-      }
-    );
+const sequelize = new Sequelize({
+  host: process.env.DB_HOST || 'localhost',
+  port: process.env.DB_PORT || 5432,
+  database: process.env.DB_NAME || 'postgres',
+  username: process.env.DB_USER || 'postgres',
+  password: process.env.DB_PASSWORD || '',
+  dialect: process.env.DB_DIALECT || 'mysql',
+  logging: false,
+  dialectOptions: process.env.DB_DIALECT === 'postgres' ? {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  } : {},
+});
 
 export default sequelize;
