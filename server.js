1e9fc3c41303d5c71510d9ae9fd0bf566a47e143
phillip847 <filippus224@gmail.com>
2025-12-05 09:32:37 +0200

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,10 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['https://diaryofan-investor.vercel.app', 'http://localhost:3000'],
+  credentials: true
+}));
 app.use(express.json());
 
 // Root route
