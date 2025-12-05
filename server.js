0285f7c2be2c819a46bf36680f33b2301d4fdd8d
phillip847 <filippus224@gmail.com>
2025-12-05 08:49:07 +0200

    Update server.js

--- server.js
+++ server.js
@@ -14,6 +14,11 @@ const PORT = process.env.PORT || 3001;
 app.use(cors());
 app.use(express.json());
 
+// Root route
+app.get('/', (req, res) => {
+  res.json({ message: 'Diary Investor API', status: 'running' });
+});
+
 // Routes
 app.use('/api/articles', articlesRouter);
 app.use('/api/pages', pagesRouter);
