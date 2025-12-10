829d1db3fa37a2dab010580a7b1815f3bba28be0
phillip847 <filippus224@gmail.com>
2025-12-10 12:07:11 +0200

    Update subscribe.js

--- api/newsletter/subscribe.js
+++ api/newsletter/subscribe.js
@@ -3,15 +3,21 @@ import { Subscriber } from '../../models/Newsletter.js';
 import sgMail from '@sendgrid/mail';
 
 export default async function handler(req, res) {
+  console.log('=== NEWSLETTER SUBSCRIBE FUNCTION CALLED ===');
+  console.log('Method:', req.method);
+  console.log('Body:', req.body);
+  
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   
   if (req.method === 'OPTIONS') {
+    console.log('OPTIONS request handled');
     return res.status(200).end();
   }
 
   if (req.method !== 'POST') {
+    console.log('Invalid method:', req.method);
     return res.status(405).json({ error: 'Method not allowed' });
   }
 
