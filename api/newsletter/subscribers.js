226cbd8f2d6a1a2aef53de9700453be01f6818f9
phillip847 <filippus224@gmail.com>
2025-12-10 10:43:02 +0200

    Create subscribers.js

---
+++ api/newsletter/subscribers.js
@@ -0,0 +1,25 @@
+import connectDB from '../../config/database.js';
+import { Subscriber } from '../../models/Newsletter.js';
+
+export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
+  if (req.method !== 'GET') {
+    return res.status(405).json({ error: 'Method not allowed' });
+  }
+
+  try {
+    await connectDB();
+    
+    const subscribers = await Subscriber.find().sort({ createdAt: -1 });
+    res.json(subscribers);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
