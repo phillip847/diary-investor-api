0d9e2e2e910722dc190cee9f20ae34ca81dc24c0
phillip847 <filippus224@gmail.com>
2025-12-10 10:11:11 +0200

    Update list.js

--- api/newsletter/list.js
+++ api/newsletter/list.js
@@ -1,7 +1,8 @@
 export default async function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'https://diaryofan-investor.vercel.app');
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
   
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
