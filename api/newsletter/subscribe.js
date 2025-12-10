3c29481bee3a5b23c456ac8bf3e994fc74a766d7
phillip847 <filippus224@gmail.com>
2025-12-10 10:11:30 +0200

    Update subscribe.js

--- api/newsletter/subscribe.js
+++ api/newsletter/subscribe.js
@@ -9,9 +9,10 @@ const newsletterSchema = new mongoose.Schema({
 const Newsletter = mongoose.models.Newsletter || mongoose.model('Newsletter', newsletterSchema);
 
 export default async function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'https://diaryofan-investor.vercel.app');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
   
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
